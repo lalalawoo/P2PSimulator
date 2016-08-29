0d1698646bb7b5ce20f9b83db34ffea806dad8e4
lalalawoo <lalalawoo2004@hotmail.com>
2016-08-29 16:34:38 -0700

    update RF calculation function

--- Replication.java
+++ Replication.java
@@ -1,15 +1,29 @@
 package P2PSimulator;
 
+
 public class Replication {
 	
-	private double reliability;
-	private int observationLength = 10;
+	private static double reliability = 0.9;
+	private static int minReplicationFactor = 2;
+	private static int maxReplicationFactor = 100;
 	
-	public Replication observationLength(int observationLength){
-		this.observationLength = observationLength;
-		return this;
-	}
-	public int observationLenth(){
-		return observationLength;
+	public static int replicationFactor(int m, int n) {
+		// rf = replication factor
+		for (int rf = minReplicationFactor; rf < maxReplicationFactor; rf++) {
+			double p = 1.0;
+			for (int i = 0; i < rf; i++) {
+				p = p * (m - i) / (n - i);
+			}
+			// 1 − p gives the probability of f peers not being within predicted
+			// departing m peers. As automatic replication mechanism is supposed
+			// to provide at least reliability r,
+			// 1 − p should be more than or equal to r
+			//
+			// 1-p >= r
+			if (1 - p >= reliability) {
+				return rf;
+			}
+		}
+		return maxReplicationFactor;
 	}
 }
