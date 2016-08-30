c27956072b786e7f27d8982d80c2fd954aa97062
lalalawoo <lalalawoo2004@hotmail.com>
2016-08-30 12:03:45 -0700

    PIDFE added

--- MySimulator.java
+++ MySimulator.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 public class MySimulator{
 	private static int mDataPoints = 500;
 	private static int[] mOLs = {5,10,15,20,30,40,50,60};
-	private static int peerSize = 500;
-	private static int mChurnFluctuation = 20;
-	private static int mInitChurn = 100;
+	private static int peerSize = 1000;
+	private static int mChurnFluctuation = 15;
+	private static int mInitChurn = 150;
 		
 	public static void main(String[] args){
 		
@@ -19,15 +19,19 @@ public class MySimulator{
 		ArrayList<ArrayList<Double>> smas = new ArrayList<ArrayList<Double>>();
 		ArrayList<ArrayList<Double>> emas = new ArrayList<ArrayList<Double>>();
 		ArrayList<ArrayList<Double>> demas = new ArrayList<ArrayList<Double>>();
+		ArrayList<ArrayList<Double>> pidfes = new ArrayList<ArrayList<Double>>();
 		int[] predRF_sma = new int[mOLs.length];
 		int[] predRF_ema = new int[mOLs.length];
 		int[] predRF_dema = new int[mOLs.length];
+		int[] predRF_pidfe = new int[mOLs.length];
 		int[] validCount_sma = new int[mOLs.length];
 		int[] validCount_ema = new int[mOLs.length];
 		int[] validCount_dema = new int[mOLs.length];
+		int[] validCount_pidfe = new int[mOLs.length];
 		int[] win_sma = new int[mOLs.length];
 		int[] win_ema = new int[mOLs.length];
 		int[] win_dema = new int[mOLs.length];
+		int[] win_pidfe = new int[mOLs.length];
 		while(count<mDataPoints){
 
 			int leave = c.generate(lastChurnSize);
@@ -45,22 +49,26 @@ public class MySimulator{
 					smas.add(new ArrayList<Double>());
 					emas.add(new ArrayList<Double>());
 					demas.add(new ArrayList<Double>());
+					pidfes.add(new ArrayList<Double>());
 				}
 				if(leavingChurns.size()<=OL){
 					smas.get(i).add(Double.valueOf(0));
 					emas.get(i).add(Double.valueOf(0));
 					demas.get(i).add(Double.valueOf(0));
+					pidfes.get(i).add(Double.valueOf(0));
 					continue;
 				}
 				ArrayList<Double> currSmas = smas.get(i);
 				ArrayList<Double> currEmas = emas.get(i);
 				ArrayList<Double> currDemas = demas.get(i);
+				ArrayList<Double> currPidfes = pidfes.get(i);
 				
 				// compare previous predicted value with current churn number
 				if(count>=OL){
-					if(Math.abs(currSmas.get(currSmas.size()-1)-leave)<5) win_sma[i]++;
-					if(Math.abs(currEmas.get(currEmas.size()-1)-leave)<5) win_ema[i]++;
-					if(Math.abs(currDemas.get(currDemas.size()-1)-leave)<5) win_dema[i]++;
+					if(Math.abs(currSmas.get(currSmas.size()-1)-leave)<15) win_sma[i]++;
+					if(Math.abs(currEmas.get(currEmas.size()-1)-leave)<15) win_ema[i]++;
+					if(Math.abs(currDemas.get(currDemas.size()-1)-leave)<15) win_dema[i]++;
+					if(Math.abs(currPidfes.get(currPidfes.size()-1)-leave)<15) win_pidfe[i]++;
 				}
 				
 				double predSma = pred.sma(leavingChurns,currSmas,OL);
@@ -69,6 +77,9 @@ public class MySimulator{
 				emas.get(i).add(predEma);
 				double predDema = pred.dema(leavingChurns,currDemas,OL);
 				demas.get(i).add(predDema);
+				double predPidfe = pred.pidfe(leavingChurns, currPidfes, OL);
+				pidfes.get(i).add(predPidfe);
+//				System.out.println("OL:"+OL+" prediction"+(count+1)+" - pidfe:"+predPidfe);
 //				System.out.println("OL:"+OL+" prediction"+(count+1)+" - sma:"+predSma+" ema:"+predEma+" dema:"+predDema);
 				
 				// compare previous predicted RF then calculate new 
@@ -76,11 +87,14 @@ public class MySimulator{
 					if(predRF_sma[i]>=RF && predRF_sma[i]<=RF+3) validCount_sma[i]++;
 					if(predRF_ema[i]>=RF && predRF_ema[i]<=RF+3) validCount_ema[i]++;
 					if(predRF_dema[i]>=RF && predRF_dema[i]<=RF+3) validCount_dema[i]++;
+					if(predRF_pidfe[i]>=RF && predRF_pidfe[i]<=RF+3) validCount_pidfe[i]++;
+//					System.out.println("Last prefRF_pidfe is "+predRF_pidfe[i]+". curr RF is "+RF);
 //					System.out.println("OL:"+OL+"RF_sma:"+validCount_sma[i]+" RF_ema:"+validCount_ema[i]+" RF_dema:"+validCount_dema[i]);
 				}				
 				predRF_sma[i]= Replication.replicationFactor((int)predSma, peerSize);
 				predRF_ema[i]= Replication.replicationFactor((int)predEma, peerSize);
 				predRF_dema[i]= Replication.replicationFactor((int)predDema, peerSize);
+				predRF_pidfe[i]= Replication.replicationFactor((int)predPidfe, peerSize);
 			}
 			count++;
 		}
@@ -92,11 +106,14 @@ public class MySimulator{
 		System.out.println("Predictions using EMA created.");
 		FileUtil.createCSVFile(demas, "demas");
 		System.out.println("Predictions using DEMA created.");
+		FileUtil.createCSVFile(pidfes, "pidfes");
+		System.out.println("Predictions using PIDFE created.");		
 		
 		ArrayList<ArrayList<Double>> winPercent = new ArrayList<ArrayList<Double>>();
 		winPercent.add(dataProcessHelper(win_sma));
 		winPercent.add(dataProcessHelper(win_ema));
-		winPercent.add(dataProcessHelper(win_dema));		
+		winPercent.add(dataProcessHelper(win_dema));
+		winPercent.add(dataProcessHelper(win_pidfe));
 		FileUtil.createCSVFile(winPercent, "winPercent");
 		System.out.println("Win percentage has been calculated.");
 		
@@ -104,6 +121,7 @@ public class MySimulator{
 		rfAccuracy.add(dataProcessHelper(validCount_sma));
 		rfAccuracy.add(dataProcessHelper(validCount_ema));
 		rfAccuracy.add(dataProcessHelper(validCount_dema));
+		rfAccuracy.add(dataProcessHelper(validCount_pidfe));
 		FileUtil.createCSVFile(rfAccuracy, "rfAccuracy");
 		System.out.println("RF accuracy has been calculated.");		
 	}
